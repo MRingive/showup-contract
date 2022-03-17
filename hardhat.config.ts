a8d53c923b3533f2dda4e943b9553189e8e5f53e
MRingive <mathiasringive@hotmail.com>
2022-03-17 17:22:43 +0100

    Network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import "hardhat-gas-reporter";
 import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import { network } from "hardhat";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -16,5 +17,11 @@ module.exports = {
     coinmarketcap: "e1ff3967-4974-44e4-8d9e-cbdaf6cfdeaf",
     gasPriceApi: "https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice",
     token: "MATIC"
+  },
+
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
   }
-};
+}
