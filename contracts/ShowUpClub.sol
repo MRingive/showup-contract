ed20e85caace1803ed2c181c96ad7843e6ae7406
MRingive <mathiasringive@hotmail.com>
2022-04-02 10:04:34 +0200

    Complete Journey

--- contracts/ShowUpClub.sol
+++ contracts/ShowUpClub.sol
@@ -2,8 +2,10 @@
 
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/security/PullPayment.sol";
+
 /// @title The Show Up Club.
-contract ShowUpClub {
+contract ShowUpClub is PullPayment {
 
     enum Action { Run, Walk, WorkOnAProject, Write, Program, WorkOut }
     enum Format { Minutes, Kilometers, Miles, Times }
@@ -11,16 +13,21 @@ contract ShowUpClub {
     struct Journey {
         Action action;      // Action.
         Format format;      // Format.
-        uint duration;      // Duration in ms.
+        uint duration;      // Duration in days.
         uint dailyValue;    // Daily value. E.g, 20
         string description; // Description of the journey.
         address creator;    // Journey Creator.
+        address charity;    // Charity Address.
         uint startDate;     // Start date.
         uint currentValue;  // Current value.
+        uint fundsLocked;   // Funds locked.
+        bool completed;     // Completed.
     }
 
     event JourneyCreated(address indexed creator, uint id);
+    event JourneyCompleted(uint indexed id);
     event ShowUp(uint indexed journeyId, uint value, string note);
+    
 
     // All Journeys
     Journey[] public journeys;
@@ -40,14 +47,31 @@ contract ShowUpClub {
     /// Journey ended.
     error JourneyEnded();
 
-    constructor() {} // TODO? 
+    /// Journey not ended yet.
+    error JourneyNotEnded();
+
+    /// Journey already completed.
+    error JourneyAlreadyCompleted();
+
+    constructor() {} // TODO?
 
     function createJourney(
         Action action,
         Format format,
         uint duration,
         uint dailyValue,
-        string calldata description) external {
+        string calldata description,
+        address charity) external payable {
+            _createJourney(action, format, duration, dailyValue, description, charity);
+    }
+
+    function _createJourney(
+        Action action,
+        Format format,
+        uint duration,
+        uint dailyValue,
+        string calldata description,
+        address charity) internal {
         // TODO See if push directly saves gas?
         Journey memory journey = Journey({ 
             action: action,
@@ -56,8 +80,11 @@ contract ShowUpClub {
             dailyValue: dailyValue,
             description: description,
             creator: msg.sender,
+            charity: charity,
             startDate: block.timestamp,
-            currentValue: 0
+            currentValue: 0,
+            fundsLocked: msg.value,
+            completed: false
         });
 
         journeys.push(journey);
@@ -91,13 +118,13 @@ contract ShowUpClub {
     }
 
     function showUp(uint journeyId, uint value, string calldata note) external {
-        Journey storage journey = journeys[journeyId];
-        if (journey.creator != msg.sender)
-            revert NotCreatorOfJourney();
-
         uint journeyEndDate = getJourneyEndDate(journeyId);
         if (journeyEndDate < block.timestamp)
             revert JourneyEnded();
+        
+        Journey storage journey = journeys[journeyId];
+        if (journey.creator != msg.sender)
+            revert NotCreatorOfJourney();
 
         journey.currentValue = journey.currentValue + value;
         emit ShowUp(journeyId, value, note);
@@ -107,6 +134,50 @@ contract ShowUpClub {
         returns (uint endDate_)
     {
         Journey memory journey = getJourneyInternal(journeyId);
-        endDate_ = journey.startDate + journey.duration;
+        endDate_ = journey.startDate + _getJourneyDurationInSeconds(journey);
+    }
+
+    function _getJourneyDurationInSeconds(Journey memory journey) internal pure
+        returns (uint durationInDays_)
+    {
+        durationInDays_ = journey.duration * 1 days;
+    }
+
+
+    /*
+    // Setup graph index so we can track:
+    // Journeys ended but not completed.
+        // This can be queried if we have journey data and current block nr. 
+        // We just need journeys whose end date is past.
+    // Journeys completed but not paid. <--- how to track this?
+        // Journey can be marked completed once it is past due date.
+        // How do we find journeys past due date?
+            One way could maybe be to create an entiry with "id" being the end date + journey id.
+            For each block we then look up using block time and journey id (for all journeys!)
+    */
+
+    // TODO: consider that this method may be target for automation. I.e, I call it for completed journeys.
+    function completeJourney(uint journeyId) public {
+        uint journeyEndDate = getJourneyEndDate(journeyId); // TODO: is it cheaper to use the storage journey?
+        if (journeyEndDate >= block.timestamp)
+            revert JourneyNotEnded();
+
+        Journey storage journey = journeys[journeyId];
+
+        if (journey.completed)
+            revert JourneyAlreadyCompleted();
+
+        journey.completed = true;
+
+        uint totalValueRequired = journey.duration * journey.dailyValue;
+        bool success = journey.currentValue >= totalValueRequired;
+
+        if (success) {
+            _asyncTransfer(journey.creator, journey.fundsLocked);
+        } else {
+            _asyncTransfer(journey.charity, journey.fundsLocked);
+        }
+        
+        emit JourneyCompleted(journeyId);
     }
 }
--- test/ShowUpClub.ts
+++ test/ShowUpClub.ts
@@ -17,7 +17,8 @@ describe("ShowUpClub contract", function () {
         format: 2,
         duration: 3,
         dailyValue: 4,
-        description: "A"
+        description: "A",
+        charity: "0x2Fa4C9EA2c8E7778bEF5dE33b0E5838f12606A02"
     }
 
     const journeyB = {
@@ -25,7 +26,8 @@ describe("ShowUpClub contract", function () {
         format: 3,
         duration: 7,
         dailyValue: 8,
-        description: "B"
+        description: "B",
+        charity: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
     }
 
     beforeEach(async function () {
@@ -38,19 +40,22 @@ describe("ShowUpClub contract", function () {
 
     async function createJourneyA() {
         await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
+            journeyA.action, journeyA.format, journeyA.duration,
+            journeyA.dailyValue, journeyA.description, journeyA.charity
         )
     }
 
     async function createJourneyB() {
         await hardhatShowUpClub.createJourney(
-            journeyB.action, journeyB.format, journeyB.duration, journeyB.dailyValue, journeyB.description
+            journeyB.action, journeyB.format, journeyB.duration,
+            journeyB.dailyValue, journeyB.description, journeyB.charity
         )
     }
     
     it("should create journey and emit", async function () {
         await expect(hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
+            journeyA.action, journeyA.format, journeyA.duration,
+            journeyA.dailyValue, journeyA.description, journeyA.charity
         ))
         .to.emit(hardhatShowUpClub, 'JourneyCreated')
         .withArgs(owner.address, 0);
@@ -58,22 +63,43 @@ describe("ShowUpClub contract", function () {
 
     it("should create two journeys and emit", async function () {
         await expect(hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
+            journeyA.action, journeyA.format, journeyA.duration,
+            journeyA.dailyValue, journeyA.description, journeyA.charity
         ))
         .to.emit(hardhatShowUpClub, 'JourneyCreated')
         .withArgs(owner.address, 0);
 
         await expect(hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
+            journeyA.action, journeyA.format, journeyA.duration,
+            journeyA.dailyValue, journeyA.description, journeyA.charity
         ))
         .to.emit(hardhatShowUpClub, 'JourneyCreated')
         .withArgs(owner.address, 1);
     });
 
+    it("should not create for invalid charity address", async function () {
+        await expect(hardhatShowUpClub.createJourney(
+            journeyA.action, journeyA.format, journeyA.duration,
+            journeyA.dailyValue, journeyA.description, "abcde"
+        )).to.be.reverted
+    });
+
+    it("should not create for invalid action", async function () {
+        await expect(hardhatShowUpClub.createJourney(
+            29229, journeyA.format, journeyA.duration,
+            journeyA.dailyValue, journeyA.description, journeyA.charity
+        )).to.be.reverted
+    });
+
+    it("should not create for invalid format", async function () {
+        await expect(hardhatShowUpClub.createJourney(
+            journeyA.action, 133777777, journeyA.duration,
+            journeyA.dailyValue, journeyA.description, journeyA.charity
+        )).to.be.reverted
+    });
+
     it("should create and get journey", async function () {
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
+        await createJourneyA()
   
         const journey = await hardhatShowUpClub.getJourney(0);
 
@@ -84,20 +110,19 @@ describe("ShowUpClub contract", function () {
         expect(journey.duration, "Duration").to.equal(journeyA.duration);
         expect(journey.dailyValue, "Daily value").to.equal(journeyA.dailyValue);
         expect(journey.description, "Description").to.equal(journeyA.description);
-        expect(journey.startDate, "Start Date").to.equal(latestBlock.timestamp);
         expect(journey.creator).to.equal(owner.address);
+        expect(journey.charity).to.equal(journeyA.charity);
+        expect(journey.startDate, "Start Date").to.equal(latestBlock.timestamp);
+        expect(journey.currentValue, "Current Value").to.equal(0);
+        expect(journey.fundsLocked, "Funds locked").to.equal(0);
     });
 
     it("should create and get two journeys", async function () {
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
+        await createJourneyA()
 
         const latestBlockA = await ethers.provider.getBlock("latest");
 
-        await hardhatShowUpClub.createJourney(
-            journeyB.action, journeyB.format, journeyB.duration, journeyB.dailyValue, journeyB.description
-        );
+        await createJourneyB()
 
         const latestBlockB = await ethers.provider.getBlock("latest");
   
@@ -109,23 +134,27 @@ describe("ShowUpClub contract", function () {
         expect(journeyResultA.duration, "Duration").to.equal(journeyA.duration);
         expect(journeyResultA.dailyValue, "Daily value").to.equal(journeyA.dailyValue);
         expect(journeyResultA.description, "Description").to.equal(journeyA.description);
-        expect(journeyResultA.startDate, "Start Date").to.equal(latestBlockA.timestamp);
         expect(journeyResultA.creator).to.equal(owner.address);
+        expect(journeyResultA.charity).to.equal(journeyA.charity);
+        expect(journeyResultA.startDate, "Start Date").to.equal(latestBlockA.timestamp);
+        expect(journeyResultA.currentValue, "Current Value").to.equal(0);
+        expect(journeyResultA.fundsLocked, "Funds locked").to.equal(0);
 
         expect(journeyResultB.action, "Action").to.equal(journeyB.action);
         expect(journeyResultB.format, "Format").to.equal(journeyB.format);
         expect(journeyResultB.duration, "Duration").to.equal(journeyB.duration);
         expect(journeyResultB.dailyValue, "Daily value").to.equal(journeyB.dailyValue);
         expect(journeyResultB.description, "Description").to.equal(journeyB.description);
-        expect(journeyResultB.startDate, "Start Date").to.equal(latestBlockB.timestamp);
         expect(journeyResultB.creator).to.equal(owner.address);
+        expect(journeyResultB.charity).to.equal(journeyB.charity);
+        expect(journeyResultB.startDate, "Start Date").to.equal(latestBlockB.timestamp);
+        expect(journeyResultB.currentValue, "Current Value").to.equal(0);
+        expect(journeyResultB.fundsLocked, "Funds locked").to.equal(0);
     });
 
 
     it("should get journey id", async function () {
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
+        await createJourneyA()
   
         const journeys = await hardhatShowUpClub.getJourneyIds();
 
@@ -134,12 +163,8 @@ describe("ShowUpClub contract", function () {
     });
 
     it("should get journey ids", async function () {
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
+        await createJourneyA()
+        await createJourneyA()
   
         const journeys = await hardhatShowUpClub.getJourneyIds();
 
@@ -149,15 +174,12 @@ describe("ShowUpClub contract", function () {
     });
 
     it("two accounts should get journey ids. Third should get none", async function () {
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
+        await createJourneyA()
         await hardhatShowUpClub.connect(addr1).createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
+            journeyA.action, journeyA.format, journeyA.duration,
+            journeyA.dailyValue, journeyA.description, journeyA.charity
         );
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
+        await createJourneyA()
   
         const journeys = await hardhatShowUpClub.getJourneyIds();
 
@@ -176,12 +198,8 @@ describe("ShowUpClub contract", function () {
     });
 
     it("should get journey ids for user", async function () {
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
+        await createJourneyA()
+        await createJourneyA()
   
         const journeys = await hardhatShowUpClub.getJourneyIdsForUser(owner.address);
 
@@ -191,12 +209,8 @@ describe("ShowUpClub contract", function () {
     });
 
     it("should not get journey ids for different user", async function () {
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
+        await createJourneyA()
+        await createJourneyA()
   
         const journeys = await hardhatShowUpClub.getJourneyIdsForUser(await addr1.getAddress());
 
@@ -204,15 +218,12 @@ describe("ShowUpClub contract", function () {
     });
 
     it("should get journey ids for different users", async function () {
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
+        await createJourneyA()
         await hardhatShowUpClub.connect(addr1).createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
+            journeyA.action, journeyA.format, journeyA.duration,
+            journeyA.dailyValue, journeyA.description, journeyA.charity
         );
-        await hardhatShowUpClub.createJourney(
-            journeyA.action, journeyA.format, journeyA.duration, journeyA.dailyValue, journeyA.description
-        );
+        await createJourneyA()
   
         const journeys = await hardhatShowUpClub.getJourneyIdsForUser(owner.address);
 
@@ -247,7 +258,7 @@ describe("ShowUpClub contract", function () {
 
             const latestBlockB = await ethers.provider.getBlock("latest");
 
-            await ethers.provider.send("evm_mine", [latestBlockB.timestamp + journeyA.duration - 1]);
+            await ethers.provider.send("evm_mine", [latestBlockB.timestamp + (journeyA.duration * 86400) - 1]);
 
             await hardhatShowUpClub.showUp(0, 25, "a note?")
         })
@@ -257,7 +268,7 @@ describe("ShowUpClub contract", function () {
 
             const latestBlockB = await ethers.provider.getBlock("latest");
 
-            await ethers.provider.send("evm_mine", [latestBlockB.timestamp + journeyA.duration]);
+            await ethers.provider.send("evm_mine", [latestBlockB.timestamp + (journeyA.duration * 86400)]);
 
             await expect(hardhatShowUpClub.showUp(0, 25, "a note?")).to.be.reverted
         })
@@ -316,6 +327,134 @@ describe("ShowUpClub contract", function () {
             .to.emit(hardhatShowUpClub, 'ShowUp')
             .withArgs(1, 30, "another note?");
         });
+    });
+
+    describe("Complete journey", function () {
+
+        it("should revert for journey not found", async () => {
+            await expect(hardhatShowUpClub.completeJourney(0)).to.be.reverted
+        });
+
+        it("should revert for journey not ended yet", async () => {
+            await createJourneyA();
+
+            const latestBlock = await ethers.provider.getBlock("latest");
+
+            await ethers.provider.send("evm_mine", [latestBlock.timestamp + (journeyA.duration * 86400) - 1]);
+
+            await expect(hardhatShowUpClub.completeJourney(0)).to.be.reverted
+        });
+
+        it("should complete for journey ended", async () => {
+            await createJourneyA();
+
+            const latestBlock = await ethers.provider.getBlock("latest");
+
+            await ethers.provider.send("evm_mine", [latestBlock.timestamp + (journeyA.duration * 86400)]);
+
+            await hardhatShowUpClub.completeJourney(0)
+        });
+
+        it("should mark journey complete", async () => {
+            await createJourneyA();
+
+            const latestBlock = await ethers.provider.getBlock("latest");
+            await ethers.provider.send("evm_mine", [latestBlock.timestamp + (journeyA.duration * 86400)]);
+
+            const journeyBefore = await hardhatShowUpClub.getJourney(0)
+            
+            expect(journeyBefore.completed).to.equal(false)
+
+            await hardhatShowUpClub.completeJourney(0)
+
+            const journeyAfter = await hardhatShowUpClub.getJourney(0)
+            
+            expect(journeyAfter.completed).to.equal(true)
+        });
+
+        it("should revert if already completed", async () => {
+            await createJourneyA();
+
+            const latestBlock = await ethers.provider.getBlock("latest");
+            await ethers.provider.send("evm_mine", [latestBlock.timestamp + (journeyA.duration * 86400)]);
+
+            await hardhatShowUpClub.completeJourney(0)
+            await expect(hardhatShowUpClub.completeJourney(0)).to.be.reverted
+        });
+
+        it("should provide money to charity address if journey failed", async () => {
+            await hardhatShowUpClub.createJourney(
+                journeyA.action, journeyA.format, journeyA.duration,
+                journeyA.dailyValue, journeyA.description, journeyA.charity,
+                { value: 123 }
+            )
+
+            await hardhatShowUpClub.showUp(0, journeyA.dailyValue * journeyA.duration - 1, "a note?")
+
+            const latestBlock = await ethers.provider.getBlock("latest");
+            await ethers.provider.send("evm_mine", [latestBlock.timestamp + (journeyA.duration * 86400)]);
+
+            await hardhatShowUpClub.completeJourney(0)
+
+            const fundsForCharity = await hardhatShowUpClub.payments(journeyA.charity)
+            const fundsForCreator = await hardhatShowUpClub.payments(owner.address)
+            
+            expect(fundsForCharity).to.equal(123)
+            expect(fundsForCreator).to.equal(0)
+        });
+
+        it("should provide money to creator address if journey succeeded", async () => {
+            await hardhatShowUpClub.createJourney(
+                journeyA.action, journeyA.format, journeyA.duration,
+                journeyA.dailyValue, journeyA.description, journeyA.charity,
+                { value: 123 }
+            )
+
+            await hardhatShowUpClub.showUp(0, journeyA.dailyValue * journeyA.duration, "a note?")
+
+            const latestBlock = await ethers.provider.getBlock("latest");
+            await ethers.provider.send("evm_mine", [latestBlock.timestamp + (journeyA.duration * 86400)]);
+
+            await hardhatShowUpClub.completeJourney(0)
+
+            const fundsForCharity = await hardhatShowUpClub.payments(journeyA.charity)
+            const fundsForCreator = await hardhatShowUpClub.payments(owner.address)
+            
+            expect(fundsForCharity).to.equal(0)
+            expect(fundsForCreator).to.equal(123)
+        });
+
+        it("should emit journey completed", async () => {
+            await createJourneyA();
+
+            const latestBlock = await ethers.provider.getBlock("latest");
+
+            await ethers.provider.send("evm_mine", [latestBlock.timestamp + (journeyA.duration * 86400)]);
+
+            await expect(hardhatShowUpClub.completeJourney(0))
+            .to.emit(hardhatShowUpClub, 'JourneyCompleted')
+            .withArgs(0);
+        });
+
+        it("should emit multiple journeys completed", async () => {
+            await createJourneyA();
+            await createJourneyB();
+
+            const latestBlock = await ethers.provider.getBlock("latest");
+
+            await ethers.provider.send("evm_mine", [latestBlock.timestamp + ((journeyA.duration + journeyB.duration) * 86400)]);
+
+            await expect(hardhatShowUpClub.completeJourney(0))
+            .to.emit(hardhatShowUpClub, 'JourneyCompleted')
+            .withArgs(0);
+
+            await expect(hardhatShowUpClub.completeJourney(1))
+            .to.emit(hardhatShowUpClub, 'JourneyCompleted')
+            .withArgs(1);
+        });
+
 
     });
+
+
   });
